5a48ac5964ec2b009ff0e017fb40987fee610fae
Stuart Gale <bishboria@gmail.com>
2012-07-20 14:52:18 +0100

    Changing it to triangle rendering.

---
+++ opengltutorial/4.hs
@@ -0,0 +1,18 @@
+import Graphics.Rendering.OpenGL
+import Graphics.UI.GLUT
+
+points :: [(GLfloat, GLfloat, GLfloat)]
+points = map (\k -> (sin (2*pi*k/12), cos (2*pi*k/12), 0.0)) [1..12]
+
+main :: IO ()
+main = do
+    (progName, _) <- getArgsAndInitialize
+    createWindow "Hello, World!"
+    displayCallback $= display
+    mainLoop
+
+display :: IO ()
+display = do
+    clear [ColorBuffer]
+    renderPrimitive Triangles $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) points
+    flush
