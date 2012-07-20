9bc70ec57e5fc389da9a0644f06f20c82109b083
Stuart Gale <bishboria@gmail.com>
2012-07-20 16:34:49 +0100

    Triagle fan

---
+++ opengltutorial/6.hs
@@ -0,0 +1,20 @@
+import Graphics.Rendering.OpenGL
+import Graphics.UI.GLUT
+
+points :: [(GLfloat, GLfloat, GLfloat)]
+points = map (\k -> (sin (2*pi*k/12), cos (2*pi*k/12), 0.0)) [1..12]
+
+main :: IO ()
+main = do
+    (progName, _) <- getArgsAndInitialize
+    createWindow "Hello, World!"
+    displayCallback $= display
+    mainLoop
+
+display :: IO ()
+display = do
+    clear [ColorBuffer]
+    -- TriangleFan uses first as base point
+    -- each subsequent pair to define a triangle with base point
+    renderPrimitive TriangleFan $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) points
+    flush
