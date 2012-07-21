7a8577b1df8ec03f8e9058bfa2b3aa302186eccf
Stuart Gale <bishboria@gmail.com>
2012-07-22 00:17:22 +0100

    Extracting points function.

--- opengltutorial/part2/Display.hs
+++ opengltutorial/part2/Display.hs
@@ -4,15 +4,13 @@ import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
 
 import Cube
-
-points :: [(GLfloat,GLfloat,GLfloat)]
-points = map (\k -> (sin(2*pi*k/12), cos(2*pi*k/12), 0.0)) [1..12]
+import Points
 
 display = do
     clear [ColorBuffer]
     mapM_ (\(x,y,z) -> preservingMatrix $ do
-        color $ Color3 x y z
+        color $ Color3 ((x+1.0)/2.0) ((y+1.0)/2.0) ((z+1)/2.0)
         translate $ Vector3 x y z
         cube (0.1::GLfloat)
-        ) points
+        ) $ points 7
     flush
