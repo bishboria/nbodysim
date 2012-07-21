8510b3a5b51a5e6561392a6c39724dc5fd527974
Stuart Gale <bishboria@gmail.com>
2012-07-21 16:23:12 +0100

    Updating QuadStrip to show colour & an explanation.

--- opengltutorial/11.hs
+++ opengltutorial/11.hs
@@ -16,5 +16,36 @@ display = do
     clear [ColorBuffer]
     -- Like TriangleStrip but window of 4 and step size of 2
     -- not sure I get how this one draws... Should add colour to it!
-    renderPrimitive QuadStrip $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) points
+    -- So, drawing it with colour such the following vertex pattern appears
+    -- 1,2,4
+    -- 1,3,4
+    -- 3,4,6
+    -- 3,5,6
+    -- 5,6,8
+    -- 5,7,8
+    -- 7,8,10
+    -- 7,9,10
+    -- 9,10,12
+    -- 9,11,12
+    -- Window size IS 4! [1..4] take 1,2,4 then 1,3,4
+    -- Then next window starts at last step ie 3,4
+    renderPrimitive QuadStrip $ do
+        color  $ Color3 (1::GLfloat) 0 0
+        vertex $ Vertex3 (0.5::GLfloat) 0.8660254 0.0
+        vertex $ Vertex3 (0.86602545::GLfloat) 0.49999997 0.0
+        color  $ Color3 (1::GLfloat) 1 0
+        vertex $ Vertex3 (1.0::GLfloat) (-4.371139e-8) 0.0
+        vertex $ Vertex3 (0.8660254::GLfloat) (-0.50000006) 0.0
+        color  $ Color3 (1::GLfloat) 1 1
+        vertex $ Vertex3 (0.49999982::GLfloat) (-0.8660255) 0.0
+        vertex $ Vertex3 (-8.742278e-8::GLfloat) (-1.0) 0.0
+        color  $ Color3 (1::GLfloat) 0 1
+        vertex $ Vertex3 (-0.5000002::GLfloat) (-0.86602527) 0.0
+        vertex $ Vertex3 (-0.86602545::GLfloat) (-0.4999999) 0.0
+        color  $ Color3 (0::GLfloat) 0 0
+        vertex $ Vertex3 (-1.0::GLfloat) 1.1924881e-8 0.0
+        vertex $ Vertex3 (-0.8660252::GLfloat) 0.50000036 0.0
+        color  $ Color3 (0::GLfloat) 0 1
+        vertex $ Vertex3 (-0.49999976::GLfloat) 0.86602557 0.0
+        vertex $ Vertex3 (1.7484555e-7::GLfloat) 1.0 0.0
     flush
