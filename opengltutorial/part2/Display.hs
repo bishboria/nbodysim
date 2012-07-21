76629b71430285c4c3570312e4c51ad2102b663c
Stuart Gale <bishboria@gmail.com>
2012-07-21 23:59:09 +0100

    Pulling out functions and rendering more cubes.

--- opengltutorial/part2/Display.hs
+++ opengltutorial/part2/Display.hs
@@ -5,7 +5,14 @@ import Graphics.UI.GLUT
 
 import Cube
 
+points :: [(GLfloat,GLfloat,GLfloat)]
+points = map (\k -> (sin(2*pi*k/12), cos(2*pi*k/12), 0.0)) [1..12]
+
 display = do
     clear [ColorBuffer]
-    cube (0.2::GLfloat)
+    mapM_ (\(x,y,z) -> preservingMatrix $ do
+        color $ Color3 x y z
+        translate $ Vector3 x y z
+        cube (0.1::GLfloat)
+        ) points
     flush
