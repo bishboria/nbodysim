8c47ca00ab4c060613e38a4a059f9a128cfd0b46
Stuart Gale <bishboria@gmail.com>
2012-07-22 15:54:33 +0100

    Beginning to add animation. Not resetting transformations.

--- opengltutorial/part2/Display.hs
+++ opengltutorial/part2/Display.hs
@@ -1,4 +1,4 @@
-module Display (display) where
+module Display (idle, display) where
 
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
@@ -6,8 +6,10 @@ import Graphics.UI.GLUT
 import Cube
 import Points
 
-display = do
+display angle = do
     clear [ColorBuffer]
+    a <- get angle
+    rotate a $ Vector3 0 0 (1::GLfloat)
     scale 0.7 0.7 (0.7::GLfloat)
     mapM_ (\(x,y,z) -> preservingMatrix $ do
         color $ Color3 ((x+1.0)/2.0) ((y+1.0)/2.0) ((z+1)/2.0)
@@ -15,3 +17,10 @@ display = do
         cube (0.1::GLfloat)
         ) $ points 7
     flush
+
+idle angle = do
+    a <- get angle
+    angle $=! (a + 0.1) -- parens necessary due to a precedence bug 
+                        -- in StateVar
+    postRedisplay Nothing -- Only required on OSX which double-buffers
+                          -- internally
--- opengltutorial/part2/Main.hs
+++ opengltutorial/part2/Main.hs
@@ -1,12 +1,15 @@
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
+import Data.IORef
 
 import Bindings
 
 main = do
     (progName,_) <- getArgsAndInitialize
     createWindow "Hello, World!"
-    displayCallback $= display
     reshapeCallback $= Just reshape
     keyboardMouseCallback $= Just keyboardMouse
+    angle <- newIORef 0.0
+    displayCallback $= (display angle)
+    idleCallback $= Just (idle angle)
     mainLoop
