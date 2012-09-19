c2100b0a7b98566c8bd09f8dcad0a4cb31a68f57
Eric Kow <eric.kow@gmail.com>
2012-09-19 19:45:05 +0100

    Miscellaneous style suggestions
    Domain naive.

--- Force.hs
+++ Force.hs
@@ -15,18 +15,17 @@ applyForcesInternal t (f:fs) (p:ps) =
     applyForce t f p : applyForcesInternal t fs ps
 
 applyForce t f particle =
-    mkParticle (mass particle)
-               p_new
-               v_new
-        where
-            acc a b = V.zipWith (+) a $ V.map (*t) b
-            v       = vel particle
-            p       = pos particle
-            v_new   = acc v f
-            p_new   = acc p v_new
+    mkParticle (mass particle) p_new v_new
+  where
+    acc a b = V.zipWith (+) a $ V.map (*t) b
+    v       = vel particle
+    p       = pos particle
+    v_new   = acc v f
+    p_new   = acc p v_new
 
 calculateForces ps = calculateForcesRecursive (length ps) ps
 
+-- Is there some higher order function that you can use to refactor this?
 calculateForcesRecursive 0 _      = []
 calculateForcesRecursive n (p:ps) =
     force p ps : calculateForcesRecursive (n-1) (ps ++ [p])
@@ -34,11 +33,12 @@ calculateForcesRecursive n (p:ps) =
 force :: Particle -> [Particle] -> Force
 force _ []     = V.fromList [0,0,0]
 force p (q:ps) = V.zipWith (+) (f p q) $ force p ps
-
-f p q =
-    let relative = V.zipWith (-) (pos p) (pos q)
+  where
+    f p q =
+        V.map (*mgdr) relative
+      where
+        relative = V.zipWith (-) (pos p) (pos q)
         dr       = (sqrt . V.sum . V.map (^2)) relative
         mgdr     = -g * mass q / dr^3
-    in  V.map (*mgdr) relative
 
 g = 1.0 -- Scale gravity to 1...
